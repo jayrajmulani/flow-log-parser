ac0dc937e75c06988175f723af5ccb2c71e939af
Jayraj Mulani <jayrajm.mulani@gmail.com>
2024-08-28 11:50:00 -0400

    fix: log format

--- constants.py
+++ constants.py
@@ -8,5 +8,5 @@ PROTOCOL_MAPPING_CSV = "protocol-numbers-1.csv"
 LOOKUP_TABLE_CSV = "lookup-table.csv"
 
 # Logging configurations
-logging.basicConfig(level=logging.INFO, format='\n%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 LOG = logging.getLogger("log_parser")
